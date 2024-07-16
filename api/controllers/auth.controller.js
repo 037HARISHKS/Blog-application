fc9a085fbc018a3f109615c5eb45336138d5a27d
037HARISHKS <harish14samynathan@gmail.com>
2024-07-16 08:40:41 -0700

    signup completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,24 +1,24 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 
-export const signUp = async (req,res,next) =>{
-    const {username ,email ,password} = req.body;
-
-    if(!username || !email || !password || username === '' || email ==='' || password ===''  ){
-        return res.status(400).json({message : "All the feilds are required "})
+export const signUp = async (req, res, next) => {
+    console.log(req.body);  // Debugging line
+    const { username, email, password } = req.body;
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return res.status(400).json({ message: "All the fields are required", success: false });
     }
 
-    const hashpassword = bcryptjs.hashSync(password,10);
+    const hashpassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
         username,
         email,
-        password : hashpassword,
+        password: hashpassword,
     });
 
-    try{
+    try {
         await newUser.save();
-        res.json({message :"sign-up successfull !!"})
-    }catch(error){
+        res.json({ message: "Sign-up successful!!", success: true });
+    } catch (error) {
         next(error);
     }
 };
